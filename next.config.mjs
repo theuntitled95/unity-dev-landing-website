dac85cbbe6ff50348cec58fb1da00a84ec47754e
Talha Hussain <theuntitled95@gmail.com>
2025-04-08 15:48:12 +0400

    add env config to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
-let userConfig = undefined
+let userConfig = undefined;
 try {
   // try to import ESM first
-  userConfig = await import('./v0-user-next.config.mjs')
+  userConfig = await import("./v0-user-next.config.mjs");
 } catch (e) {
   try {
     // fallback to CJS import
@@ -27,25 +27,31 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
-}
+  env: {
+    HUBSPOT_PORTAL_ID: process.env.HUBSPOT_PORTAL_ID,
+    HUBSPOT_FORM_GUID: process.env.HUBSPOT_FORM_GUID,
+    ZOHO_ACCESS_TOKEN: process.env.ZOHO_ACCESS_TOKEN,
+    ZOHO_FORM_LINK_NAME: process.env.ZOHO_FORM_LINK_NAME,
+  },
+};
 
 if (userConfig) {
   // ESM imports will have a "default" property
-  const config = userConfig.default || userConfig
+  const config = userConfig.default || userConfig;
 
   for (const key in config) {
     if (
-      typeof nextConfig[key] === 'object' &&
+      typeof nextConfig[key] === "object" &&
       !Array.isArray(nextConfig[key])
     ) {
       nextConfig[key] = {
         ...nextConfig[key],
         ...config[key],
-      }
+      };
     } else {
-      nextConfig[key] = config[key]
+      nextConfig[key] = config[key];
     }
   }
 }
 
-export default nextConfig
+export default nextConfig;
